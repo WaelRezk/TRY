eddc939379ebad7721bb56522ba9be9c543f439e
waelrezk <waelnabel@live.com>
2012-12-19 13:53:58 +0200

    second

---
+++ app/controllers/dddd_controller.rb
@@ -0,0 +1,13 @@
+class DdddController < ApplicationController
+  def aa
+  end
+
+  def ss
+  end
+
+  def dd
+  end
+
+  def ff
+  end
+end
---
+++ app/controllers/sayed_controller.rb
@@ -0,0 +1,13 @@
+class SayedController < ApplicationController
+  def sayed1
+  end
+
+  def sayed2
+  end
+
+  def sayed3
+  end
+
+  def sayed4
+  end
+end
